51035aa4ae25334d118bdd802da2799c67c37517
biefaquan <biefaquan@gmail.com>
2018-06-10 17:44:10 +0800

    add the reason why why the class of ExplicitCriticalSection.java execute multithreading failed.

--- src/com/concurrent/example/ExplicitCriticalSection.java
+++ src/com/concurrent/example/ExplicitCriticalSection.java
@@ -3,6 +3,14 @@ package com.concurrent.example;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * P688
+ * Lock显示创建临界区
+ * 但是线程确实不安全的了，报错？
+ * 可能原因：ExplicitPairManager2.lock.lock()加的锁，与PairManager.getgetPair()锁的对象不是一个，
+ * 所以在p.incrementX()的时候，getPair()执行，此时x!=y
+ *
+ */
 class ExplicitPairManager1 extends PairManager {
     private Lock lock = new ReentrantLock();
     public synchronized void increment() {
@@ -25,6 +33,7 @@ class ExplicitPairManager2 extends PairManager {
         try{
             p.incrementX();
             p.incrementY();
+            System.out.println("x: " + p.getX() + "y: " + p.getY());
             temp = getPair();
         }finally {
             lock.unlock();
